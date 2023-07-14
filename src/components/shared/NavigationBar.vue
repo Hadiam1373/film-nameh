b0e2647fdf8649ea3996c6bbe486c92245242806
Hadi.Moradi137 <hadimoradi1373@outlook.com>
2023-07-14 18:39:55 +0330

    add starter navigation and footer

---
+++ src/components/shared/NavigationBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app-bar :elevation="2">
+    <div class="w-100 d-flex align-center justify-space-between px-3">
+      <div class="d-flex align-center">
+        <span class="font-weight-bold mr-5">Title</span>
+        <div class="mx-5 pa-3 hidden-sm-and-down">
+          <span class="mx-2">Item1</span>
+          <span class="mx-2">Item1</span>
+          <span class="mx-2">Item1</span>
+          <span class="mx-2">Item1</span>
+        </div>
+      </div>
+      <div class="d-flex align-center justify-space-between">
+        <div class="d-flex align-center mx-5">
+          <v-btn icon="mdi-heart"></v-btn>
+          <v-btn icon="mdi-magnify"></v-btn>
+          <v-btn icon="mdi-dots-vertical"></v-btn>
+        </div>
+        <v-spacer></v-spacer>
+        <v-app-bar-nav-icon class="hidden-lg hidden-md" :elevation="0"></v-app-bar-nav-icon>
+      </div>
+
+    </div>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "NavigationBar"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -1,9 +1,13 @@
 <template>
   <v-app>
-    <default-view />
+    <NavigationBar/>
+    <default-view  style="min-height:80vh;"/>
+    <Footer/>
   </v-app>
 </template>
 
 <script setup>
   import DefaultView from './View.vue'
+  import NavigationBar from "@/components/shared/NavigationBar.vue";
+  import Footer from "@/components/shared/Footer.vue";
 </script>
